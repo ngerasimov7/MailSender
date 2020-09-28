652d249dab1a8a20f97cdb46c9d259dd712d26ee
ngerasimov7 <nikola20@mail.ru>
2020-09-29 01:22:41 +0300

    Разработка интерфейса пользователя, добавление классов и библиотеки

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using System.Net;
 using System.Net.Mail;
 using System.Windows.Interop;
+using MailSender.Models;
+using MailSender.lib;
 
 namespace MailSender
 {
@@ -11,14 +13,39 @@ namespace MailSender
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Config cfg = new Config();
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnSendButtonClick(object Sender, RoutedEventArgs e)
+        {
+            if (!(SendersList.SelectedItem is Sender sender)) return;
+            if (!(RecipientsList.SelectedItem is Recipient recipient)) return;
+            if (!(ServersList.SelectedItem is Server server)) return;
+            if (!(MessagesList.SelectedItem is Message message)) return;
+
+            var send_service = new MailSenderService
+            {
+                ServerAdress = server.Address,
+                ServerPort = server.Port,
+                UseSSL = server.UseSSL,
+                Login = server.Login,
+                Password = server.Password,
+            };
+
+            try
+            {
+                send_service.SendMessage(sender.Address, recipient.Address, message.Subject, message.Body);
+            }
+            catch(SmtpException error)
+            {
+                MessageBox.Show("Ошибка при отправке почты" + error.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);)
+            }
+        }
+
+
+        /*private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -46,8 +73,9 @@ namespace MailSender
             catch
             {
                 MessageBox.Show("Опаньки! Что-то пошло не так!");
+
             }
             
-        }
+        }*/
     }
 }
